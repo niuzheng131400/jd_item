703c40dacec944781f2fdd3d9395e5e9fba6bd9d
niuzheng131400 <771036148@qq.com>
2022-09-14 17:24:07 +0800

    Create jd.sql

---
+++ jd.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE jd;
+USE jd;
+SET NAMES utf8;
+DROP TABLE IF EXISTS `jd_search`;
+CREATE TABLE `jd_search`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `sku_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `shop` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `icons` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 121 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
